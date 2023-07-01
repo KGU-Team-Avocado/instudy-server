af634036128695615fb75a13970c568945b0a6e9
Kim yeonsu <yeonsu0329@kyonggi.ac.kr>
2023-07-01 16:06:38 +0900

    Initial commit

---
+++ src/main/java/com/avocado/instudyserver/InstudyServerApplication.java
@@ -0,0 +1,13 @@
+package com.avocado.instudyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstudyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstudyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avocado/instudyserver/InstudyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avocado.instudyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstudyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
